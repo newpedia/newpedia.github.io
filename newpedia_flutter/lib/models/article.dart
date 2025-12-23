1ce8a6874f4281f51b93173eefbd2c3324c37417
newpedia <jakbapedia@gmail.com>
2025-12-23 17:25:49 +0700

    Create article.dart

---
+++ newpedia_flutter/lib/models/article.dart
@@ -0,0 +1,11 @@
+class Article {
+  final String title;
+  final String summary;
+  final String date;
+
+  Article({
+    required this.title,
+    required this.summary,
+    required this.date,
+  });
+}
