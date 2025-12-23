a6e2a8596fe3960b6739656804a3287d5f4963e4
newpedia <jakbapedia@gmail.com>
2025-12-24 00:16:43 +0700

    Create index.php

---
+++ php/themes/index.php
@@ -0,0 +1,13 @@
+<?php get_header(); ?>
+
+<div class="container">
+<?php if ( have_posts() ) : ?>
+    <?php while ( have_posts() ) : the_post(); ?>
+        <h2><?php the_title(); ?></h2>
+        <?php the_content(); ?>
+        <hr>
+    <?php endwhile; ?>
+<?php endif; ?>
+</div>
+
+<?php get_footer(); ?>
