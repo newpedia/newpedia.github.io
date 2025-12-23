d547108cd73e70ec37aefcfd4c2f0f42c6b31782
newpedia <jakbapedia@gmail.com>
2025-12-23 18:05:14 +0700

    Create ad_banner.dart

---
+++ newpedia_android/lib/ads/ad_banner.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
+
+class AdBanner extends StatefulWidget {
+  const AdBanner({super.key});
+
+  @override
+  State<AdBanner> createState() => _AdBannerState();
+}
+
+class _AdBannerState extends State<AdBanner> {
+  late BannerAd _bannerAd;
+  bool _isLoaded = false;
+
+  // ✅ TEST ID (WAJIB saat development)
+  static const String bannerAdUnitId =
+      'ca-app-pub-3940256099942544/6300978111';
+
+  @override
+  void initState() {
+    super.initState();
+    _bannerAd = BannerAd(
+      size: AdSize.banner,
+      adUnitId: bannerAdUnitId,
+      request: const AdRequest(),
+      listener: BannerAdListener(
+        onAdLoaded: (_) {
+          setState(() => _isLoaded = true);
+        },
+        onAdFailedToLoad: (ad, error) {
+          ad.dispose();
+        },
+      ),
+    )..load();
+  }
+
+  @override
+  void dispose() {
+    _bannerAd.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (!_isLoaded) return const SizedBox();
+    return SizedBox(
+      height: _bannerAd.size.height.toDouble(),
+      width: _bannerAd.size.width.toDouble(),
+      child: AdWidget(ad: _bannerAd),
+    );
+  }
+}
