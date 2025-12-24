0bc115aa2749e3d32da9d260404cba417d61c921
newpedia <jakbapedia@gmail.com>
2025-12-24 14:37:56 +0700

    Create canonical.js

---
+++ js/canonical.js
@@ -0,0 +1,8 @@
+<script>
+document.addEventListener("DOMContentLoaded", function () {
+  var link = document.createElement("link");
+  link.rel = "canonical";
+  link.href = window.location.href.split("?")[0];
+  document.head.appendChild(link);
+});
+</script>
