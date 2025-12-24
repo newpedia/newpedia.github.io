ccbfafbe5153e80baf71eb81bf70df346afcd2e6
newpedia <jakbapedia@gmail.com>
2025-12-24 13:55:56 +0700

    Create load.js

---
+++ js/load.js
@@ -0,0 +1,10 @@
+ window.addEventListener("load", function () {
+    const loader = document.getElementById("loader");
+    loader.style.opacity = "0";
+    loader.style.transition = "opacity 0.5s ease";
+
+    setTimeout(() => {
+      loader.style.display = "none";
+    }, 500);
+  });
+
