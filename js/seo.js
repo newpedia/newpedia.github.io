7ac714015009322b75bd340d4ab6e43e8914e720
newpedia <jakbapedia@gmail.com>
2025-12-24 14:37:28 +0700

    Create seo.js

---
+++ js/seo.js
@@ -0,0 +1,8 @@
+<script>
+document.addEventListener("DOMContentLoaded", function () {
+  var meta = document.createElement("meta");
+  meta.name = "robots";
+  meta.content = "index, follow";
+  document.head.appendChild(meta);
+});
+</script>
